1d19363f767465e5f6ced88720984f61e15d4d32
jagangitrepo <sb.jagankumar@gmail.com>
2019-12-08 11:07:24 +0530

    Add files via upload

---
+++ installdocker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Installing the Docker in Ubuntu: ${uname -a}"
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+echo "Installation is done"
+echo "Checking the docker Status:"
+sudo systemctl status docker
