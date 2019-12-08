f6a006d0d9d43f7a829916e4a9e061d7b70744a4
jagangitrepo <sb.jagankumar@gmail.com>
2019-12-08 15:53:30 +0530

    Update install_g++_ubuntu.sh

--- install_g++_ubuntu.sh
+++ install_g++_ubuntu.sh
@@ -3,5 +3,5 @@
 #This script will setup the g++ build env.
 #Usage : curl httpdownloadurlpath | /bin/bash
 
-sudo apt install g++
-sudo apt install build-essential
+echo "Y" | sudo apt install g++
+echo "Y" | sudo apt install build-essential
